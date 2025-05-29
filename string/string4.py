b5d43941d28558105d3fcca5f9bb23e5afdd0e90
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-29 12:29:04 +0530

    string

---
+++ string/string4.py
@@ -0,0 +1,14 @@
+name="\n"
+
+#data --> string "123" --.int -->
+
+#form -->age -->"18" --> numbner --> if numbers ->type cast
+
+print("alpha",name.isalpha())
+print(f"alnum {name.isalnum()}")
+print(f"digits {name.isdigit()}")
+print(f"lower {name.islower()}")
+print(f"lower {name.isupper()}")
+print(f"isSpace {name.isspace()}")
+print(f"isprintable {name.isprintable()}")
+
